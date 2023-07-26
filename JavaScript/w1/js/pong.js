a4d3b2bd318ba264b0230556f5545f2e1009e7b0
TheJerryCann <122393082+TheJerryCann@users.noreply.github.com>
2023-07-26 14:26:17 -0400

    Update pong.js

--- JavaScript/w1/js/pong.js
+++ JavaScript/w1/js/pong.js
@@ -8,20 +8,19 @@ var player = [new Player(), new Player()]
 player[0].pad = new Box();
 player[1].pad = new Box();
 
+var pad = [player[0].pad, player[1].pad]
+
 console.log(player)
-//hi
 
-var p1 = new Box();
-p1.w = 20
-p1.h = 150
-p1.x = 0 + p1.w/2
-p1.pad 
+pad[0].w = 20
+pad[0].h = 150
+pad[0].x = 0 + pad[0].w/2
+pad[0].pad 
 
-var p2 = new Box()
-p2.w = 20
-p2.h = 150
-p2.x = c.width-p2.w/2
-p2.color = `blue`
+pad[1].w = 20
+pad[1].h = 150
+pad[1].x = c.width-pad[1].w/2
+pad[1].color = `blue`
 
 var ball = new Box()
 ball.w= 20
@@ -37,57 +36,57 @@ function main()
 
     if(keys[`w`])
     {
-        p1.vy += -p1.force
+        pad[0].vy += -pad[0].force
     }
 
     if(keys["ArrowUp"])
     {
-        p2.vy += -p2.force
+        pad[1].vy += -pad[1].force
     }
 
     if(keys["ArrowDown"])
     {
-        p2.vy += p2.force
+        pad[1].vy += pad[1].force
     }
 
     if(keys[`s`])
     {
-        p1.vy += p1.force
+        pad[0].vy += pad[0].force
     }
 
-    p1.vy *= fy
-    p2.vy *= fy
-    p1.move()
-    p2.move()
+    pad[0].vy *= fy
+    pad[1].vy *= fy
+    pad[0].move()
+    pad[1].move()
 
     ball.move()
 
-    if(p1.y < 0+p1.h/2)
+    if(pad[0].y < 0+pad[0].h/2)
     {
-        p1.y = 0+p1.h/2
+        pad[0].y = 0+pad[0].h/2
     }
-    if(p1.y > c.height-p1.h/2)
+    if(pad[0].y > c.height-pad[0].h/2)
     {
-        p1.y = c.height-p1.h/2
-    }
-
-    if(p2.y < 0+p2.h/2)
-    {
-        p2.y = 0+p2.h/2
-    }
-    if(p2.y > c.height-p2.h/2)
-    {
-        p2.y = c.height-p2.h/2
+        pad[0].y = c.height-pad[0].h/2
     }
 
-    if(ball.collide(p1))
+    if(pad[1].y < 0+pad[1].h/2)
     {
-        ball.x = p1.x + p1.w/2 + ball.w/2
+        pad[1].y = 0+pad[1].h/2
+    }
+    if(pad[1].y > c.height-pad[1].h/2)
+    {
+        pad[1].y = c.height-pad[1].h/2
+    }
+
+    if(ball.collide(pad[0]))
+    {
+        ball.x = pad[0].x + pad[0].w/2 + ball.w/2
         ball.vx = -ball.vx;
     }
-    if(ball.collide(p2))
+    if(ball.collide(pad[1]))
     {
-        ball.x = p2.x - p2.w/2 - ball.w/2
+        ball.x = pad[1].x - pad[1].w/2 - ball.w/2
         ball.vx = -ball.vx;
     }
     if(ball.x < 0)
@@ -110,7 +109,7 @@ function main()
         ball.y = c.width - ball.h/2
         ball.vy = -ball.vy
     }
-    p1.draw()
-    p2.draw()
+    pad[0].draw()
+    pad[1].draw()
     ball.draw()
 }
